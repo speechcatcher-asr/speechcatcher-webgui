7029c451975039e439daa1fbb8234a74edc43c55
bmilde <bmilde@M>
2022-11-28 17:19:43 +0100

    fixed upload issues and added status endpoints

--- backend/speechcatcher_server.py
+++ backend/speechcatcher_server.py
@@ -1,8 +1,7 @@
 import argparse
 
 import flask
-from flask import send_file
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, flash, request, redirect, url_for, send_file, jsonify
 from werkzeug.utils import secure_filename
 from werkzeug.serving import WSGIRequestHandler
 
@@ -11,15 +10,18 @@ import tempfile
 
 from redis import Redis
 from rq import Queue
+from rq.registry import StartedJobRegistry
 
-speechcatcher_queue = Queue(connection=Redis())
+redis_conn = Redis()
+speechcatcher_queue = Queue(connection=redis_conn)
+registry = StartedJobRegistry('default', connection=redis_conn)
 
 # Todo: yaml config for everything below, or as additional params to argparse
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 app.config['SESSION_TYPE'] = 'filesystem'
-app.config['CUDA_WRAPPER'] = 'CUDA_VISIBLE_DEVICES=0'
-app.config['CUDA_LD_LIBRARY_PATH'] = 'LD_LIBRARY_PATH=/usr/local/cuda/targets/x86_64-linux/lib/'
+app.config['CUDA_WRAPPER'] = '' #'CUDA_VISIBLE_DEVICES=0'
+app.config['CUDA_LD_LIBRARY_PATH'] = '' #'LD_LIBRARY_PATH=/usr/local/cuda/targets/x86_64-linux/lib/'
 app.config['SPEECHENGINE'] = 'whisper'
 app.config['SPEECHENGINE_PARAMS'] = '--model small'
 
@@ -33,15 +35,19 @@ app.secret_key = 'secretkey'
 @app.route('/process', methods=['POST'])
 def process_file():
     # check if the post request has the file part
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
+    #if len(request.files) == 0:
+    if 'my_file' not in request.files:
+        print('Warning: No file part')
+        return 'error: no file part' #redirect(request.url)
+    
+    # TODO: allow multiple files
+    file = request.files['my_file']
+    print(f'{file=}')
     # If the user does not select a file, the browser submits an
     # empty file without a filename.
-    if file.filename == '':
-        flash('No selected file')
-        return redirect(request.url)
+    #if file.filename == '':
+    #    print('Warning: No selected file')
+    #    return 'error: no selected file' #redirect(request.url)
     # Note: might make sense to implement filter for extensions?
     if file:
         filename = secure_filename(file.filename)
@@ -54,18 +60,54 @@ def process_file():
                                                                             app.config['SPEECHENGINE'], 
                                                                             app.config['SPEECHENGINE_PARAMS'],
                                                                             app.config['CUDA_LD_LIBRARY_PATH'],
-                                                                            app.config['CUDA_WRAPPER']))
+                                                                            app.config['CUDA_WRAPPER']), description=filename)
 
-        return 'ok'
+        return '<p>File uploaded.</p>'
+
+def get_job_status_dict(job):
+    return {'description':job.description, 'status':job.get_status, 'enqueued_at':job.enqueued_at,
+            'started_at':job.started_at, 'ended_at':job.ended_at}
+
+@app.route('/status', methods=['GET'])
+def status():
+    running_job_ids = registry.get_job_ids() 
+    expired_job_ids = registry.get_expired_job_ids() 
+    queued_job_ids = registry.get_queue().job_ids
+
+    running_jobs = Job.fetch_many(running_job_ids, connection=redis)
+    running_job_dicts = [get_job_status_dict(job) for job in running_jobs]
+
+    expired_jobs = Job.fetch_many(expired_job_ids, connection=redis)
+    expired_job_dicts = [get_job_status_dict(job) for job in expired_jobs]
+
+    queued_jobs = Job.fetch_many(queued_job_ids, connection=redis)
+    queued_job_dicts = [get_job_status_dict(job) for job in queued_jobs]
+
+    return jsonify({'running': running_job_dicts, 'expired': expired_job_dicts,
+                     'queued': queued_job_dicts})
+
+@app.route('/list_outputs', methods=['GET'])
+def list_outputs():
+    folder_len = len(app.config['UPLOAD_FOLDER'])
+    vtts = glob.glob(app.config['UPLOAD_FOLDER'] + '*.vtt')
+    base_filenames = [myfile[folder_len:-4] for myfile in vtts]
+    return jsonify(base_filenames)
+
+def ensure_dir(f):
+    d = os.path.dirname(f)
+    if not os.path.exists(d):
+        os.makedirs(d)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Upload and queue server for speechcatcher jobs')
     parser.add_argument('-l', '--listen-host', default='127.0.0.1', dest='host', help='Host address to listen on.')
-    parser.add_argument('-p', '--port', default=5000, dest='port', help='Port to listen on.', type=int)
+    parser.add_argument('-p', '--port', default=6000, dest='port', help='Port to listen on.', type=int)
     parser.add_argument('--debug', dest='debug', help='Start with debugging enabled',
                         action='store_true', default=False)
 
     args = parser.parse_args()
+
+    ensure_dir(app.config['UPLOAD_FOLDER'])
 
     if args.debug:
         app.debug = True
