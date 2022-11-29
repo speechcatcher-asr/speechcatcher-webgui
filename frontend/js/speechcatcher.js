3cc4329a689df3157dd970e3b4e5728057257f48
bmilde <bmilde@M>
2022-11-29 22:17:09 +0100

    added status dot js template

--- frontend/js/speechcatcher.js
+++ frontend/js/speechcatcher.js
@@ -1,3 +1,15 @@
+var jobsTemplate = doT.template(document.getElementById('jobs-template').text);
+
+function updateStatus()
+{
+    $.get( "/status", function( data ) {
+        console.log(data);
+        var jobs_view_data = $(jobsTemplate(data));
+        console.log(jobs_view_data);
+        $('#job_view').html(jobs_view_data);
+    });
+}
+
 // https://stackoverflow.com/questions/33732655/html-file-upload-why-use-iframe
 function redirect()
 {
